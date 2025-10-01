cbd8b363d752db40eaa4c573b512c9291d5393f7
shahdkazan <kazanshahed@gmail.com>
2025-10-01 23:33:17 +0300

    Add Assignment 2 scene and configure a sphere to not fall off of the plane
    - Added Assignment 2 scene in Scenes folder
    - Added plane and sphere with Rigidbody
    - Added 4 empty GameObjects with Box Colliders to act as walls
    - Implemented PlayerController script for sphere movement (using Rigidbody.MovePosition)

---
+++ Assets/scripts/sphere_movement.cs
@@ -0,0 +1,23 @@
+
+
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5.0f;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+}
